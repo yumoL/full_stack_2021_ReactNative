e3b9c02684bc3cb3ed9b412f62747db76bf834b5
luoyumo <yumo.luo@hotmail.com>
2021-12-19 01:02:03 +0200

    10.11-12

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/client';
 import { NativeRouter } from 'react-router-native';
 import { StatusBar } from 'expo-status-bar';
 
 import Main from './src/components/Main';
+import createApolloClient from './src/utils/apolloClient';
+
+const apolloClient = createApolloClient();
 
 const App = () => {
   return (
     <>
       <NativeRouter>
-        <Main />
+        <ApolloProvider client={apolloClient}>
+          <Main />
+        </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
     </>
---
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,14 @@
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+    // Other options
+  });
+
+  return { repositories: data ? data.repositories : undefined, loading, error };
+};
+
+export default useRepositories;
